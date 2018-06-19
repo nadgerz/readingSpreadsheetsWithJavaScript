15f1874204b7455babe8caf19b26b99fac05af88
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-19 17:23:24 +0200

    protect against FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory

--- bin/run.sh
+++ bin/run.sh
@@ -15,5 +15,19 @@ ext=${filename##*.}
 
 cd ${location} || exit 3
 
-node ${filename} -f ../../data/${fname}.xls  2>&1 | tee ../../logs/${fname}.log
+# node --max-old-space-size=8192 --optimize-for-size --max-executable-size=8192  --max_old_space_size=8192 --optimize_for_size --max_executable_size=8192 node_modules/karma/bin/karma start --single-run --max_new_space_size=8192   --prod --aot
 
+now=$(date '+%Y-%m-%d_%H%M%s-%S')
+
+logFile="../../logs/${fname}-${now}.log"
+
+runtimeArgs=""
+runtimeArgs="--max-old-space-size=8192"
+runtimeArgs="--max-old-space-size=4096"
+
+node ${runtimeArgs} ${filename} -f ../../data/${fname}.xls  2>&1 | tee ${logFile}
+retVal=$?
+
+echo "LOGFILE: ${logFile}"
+
+exit ${retVal}
