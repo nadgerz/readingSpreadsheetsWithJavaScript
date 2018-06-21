564f550b50892fdbafb4566def9f2895b6ae37e6
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2018-06-21 12:15:23 +0200

    get cell value by type

--- src/java/ExcelReader.java
+++ src/java/ExcelReader.java
@@ -5,7 +5,35 @@ import java.io.IOException;
 import java.util.Iterator;
 
 public class ExcelReader {
-    public static final String SAMPLE_XLSX_FILE_PATH = "./sample-xlsx-file.xlsx";
+    public static final String SAMPLE_XLSX_FILE_PATH = "../../data//crash.xls";
+
+    private static void printCellValue(Cell cell) {
+    switch (cell.getCellTypeEnum()) {
+        case BOOLEAN:
+            System.out.print(cell.getBooleanCellValue());
+            break;
+        case STRING:
+            System.out.print(cell.getRichStringCellValue().getString());
+            break;
+        case NUMERIC:
+            if (DateUtil.isCellDateFormatted(cell)) {
+                System.out.print(cell.getDateCellValue());
+            } else {
+                System.out.print(cell.getNumericCellValue());
+            }
+            break;
+        case FORMULA:
+            System.out.print(cell.getCellFormula());
+            break;
+        case BLANK:
+            System.out.print("");
+            break;
+        default:
+            System.out.print("");
+    }
+
+    System.out.print("\t");
+}
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
 
